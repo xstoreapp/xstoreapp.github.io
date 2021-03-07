0424875960947578fe5ae6addf98bfb912f46599
dragonDScript <agomila96@gmail.com>
2021-03-07 15:51:01 +0100

    Use proper host

--- pages/docs/[...page].js
+++ pages/docs/[...page].js
@@ -17,12 +17,13 @@ const Page = ({ dangerouslySetInnerHTML, fullRoute }) => {
 
 Page.getInitialProps = async (ctx) => {
     const { page } = ctx.query
+    const { req } = ctx
     const fullRoute = typeof page == 'array' ? `${page.join("/")}` : `/${page}`
     // Fetch to this website, to a public folder called public/docs that is served
     // statically.
     // This way we don't need SSR, the website can be static, open, and it doesn't
     // need file operations.
-    const readOperation = await fetch(`http://localhost:3000/staticDocs${fullRoute}.md`)
+    const readOperation = await fetch(`http://${req.headers.host}/staticDocs${fullRoute}.md`)
     const text = await readOperation.text()
     const markdown = {__html: md(text)}
     return { dangerouslySetInnerHTML: markdown, fullRoute }
