ffdad882b763aee0986623103e6a0ea7087dacae
dragonDScript <agomila96@gmail.com>
2021-03-28 11:35:45 +0200

    Fix some things

--- components/Menu.js
+++ components/Menu.js
@@ -2,7 +2,7 @@ import styles from '../styles/Menu.module.css'
 
 function Menu() {
     return <div className={styles.menu}>
-        <a href="/" style={{width: `${40}px`, backgroundColor: 'black'}}>
+        <a href="/" style={{width: `${40}px`}}>
             <img src="/icon.svg" style={{maxHeight: `${100}%`, width: `${31}px`}} />
         </a>
         <a href="/download">Download</a>
